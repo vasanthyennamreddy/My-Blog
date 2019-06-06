5a9f67fbb09317e631513c66865e73e8df4b3935
vasanthyennamreddy <yvr10@iitbbs.ac.in>
2019-06-06 12:27:27 +0530

    added header.php

---
+++ header.php
@@ -0,0 +1,52 @@
+<head>		
+	
+	<style type="text/css">
+		*{box-sizing: border-box;
+		    font-family: Arial;
+		}
+		ul{
+			list-style-type: none;
+			padding: 0;
+			margin: 0px 18.5vw 20px  18.5vw ;
+			position: absolute;
+			z-index: 0;
+			position: fixed;
+			top: 0px;
+
+		}
+		li{
+			float: left;
+			display: inline; 
+			text-align: center;
+			font-size: 23px;
+			color: white;
+			width: 15vw;
+			height: 65px;
+			padding-top: 20px;
+			margin:0px 2px ;
+
+
+		}
+		a:hover{
+			color:black;
+
+		}
+		a{
+			text-decoration: none;
+			color: white;
+		}
+		
+			
+
+		
+	</style>
+</head>
+<body>
+	
+   <ul>
+   	<li  style="background-color: rgba(190,20,20,0.5);"><a href="project.php">Home </a></li>
+   	<li  style="background-color: rgba(0,50,190,0.7);"><a href="test.php" >About Me</a> </li>
+   	<li  style="background-color: rgba(20,190,80,0.5);"><a href="skills.php">My Skills</a> </li>
+   	<li  style="background-color: rgba(10,150,230,0.9);"><a href="test1.php">Contact Me </a></li>
+   </ul>
+
